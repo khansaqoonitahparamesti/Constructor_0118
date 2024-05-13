924f1010f5cc1c128fa51de078be96aaa0f6638a
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-05-13 14:56:41 +0700

    mencetak objek

--- Constructor_0118/Constructor_0118.cpp
+++ Constructor_0118/Constructor_0118.cpp
@@ -40,4 +40,10 @@ int main() {
 	mahasiswa mhs2("Asroni");
 	mahasiswa mhs3("Andi Kurniawan");
 	mahasiswa mhs4("Joko Purba");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
 }
