ef395149375ff49b5fc080349a5979f624bf8616
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-05-13 18:00:21 +0700

    menuliskan class

---
+++ MbrStatic2/MbrStatic2.cpp
@@ -0,0 +1,10 @@
+// MbrStatic2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+
+};
