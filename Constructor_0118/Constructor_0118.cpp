1f715c0cbf8a0cfa691877df1ba9edff5ed7ee72
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-05-13 14:54:39 +0700

    menambahkan objek

--- Constructor_0118/Constructor_0118.cpp
+++ Constructor_0118/Constructor_0118.cpp
@@ -28,8 +28,16 @@ void mahasiswa::setID()
 	id = ++nim;
 }
 
-void mahasiswa::printAll() {
+void mahasiswa::printAll() 
+{
 	cout << "ID =" << id << endl;
 	cout << "Nama =" << nama << endl;
 	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mahasiswa mhs3("Andi Kurniawan");
+	mahasiswa mhs4("Joko Purba");
 }
